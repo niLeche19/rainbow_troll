57bed34da16123f51ae7f63061ce515662ecf93d
niLeche19 <nicholaslee@hanovernorwichschools.org>
2020-11-15 11:34:25 -0500

    progress

--- sixTroll.py
+++ sixTroll.py
@@ -1,36 +1,30 @@
+import string
 import subprocess
 import sys
 from time import sleep
 cmd = 'WMIC PROCESS get Caption'
 proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
 
-for line in proc.stdout:
-    print(line)
-
-sleep(2)
+strTest = ""
+recurse = 0
 
 def getName(linee):
+    strOut = ""
+
     for i in linee:
-        
-"""
+        if(chr(i).isalpha()):
+            strOut += chr(i)
+        else:
+            print(strOut)
+            break
+
+
 for line in proc.stdout:
-    print (line)
-
-import sys
-from time import sleep
-from os import system
-
-sys.path.append("D:\Python stuff\Lib\site-packages")
-import pyautogui as PAG
-
-PAG.sleep = 0.1
-
-print(sys.argv[0])
-
-
-system('cmd /k "tasklist > D:\Downloads\task_list.txt"')
-#os.system('cmd /k "Your Command Prompt Command"')
-"""
-
+    strTest = line
+    if (recurse > 2):
+        break
+    recurse += 1
+    getName(strTest)
+    print(strTest)
 
 
